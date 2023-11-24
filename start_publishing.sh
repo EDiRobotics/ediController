66f6819d9c509b3032b40c38dd9611adfa5d0f34
radiance-nt <1808154289@qq.com>
2023-11-24 15:56:44 +0800

    <update> update

--- start_publishing.sh
+++ start_publishing.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+pkill -9 ros
+echo "Killing previous ros programs..."
+sleep 2
 source /opt/ros/noetic/setup.bash
 export ROS_HOSTNAME=192.168.1.240; export ROS_MASTER_URI=http://192.168.1.240:11311
+echo "Set ROS_HOSTNAME=${ROS_HOSTNAME}, ROS_MASTER_URI=${ROS_MASTER_URI}..."
+
+echo "Starting roscore..."
 roscore &
 
-sleep 5
-
+sleep 3
 camera_numbers=$(ls /dev/video* | sed 's/[^0-9]*//g')
-
+echo "Starting obtain camera images..."
 for camera_number in $camera_numbers
 do
     /usr/bin/python3 get_images.py _camera_number:=$camera_number &
 done
 
+echo "Starting obtain arm status..."
 /usr/bin/python3 get_status.py &
 
 wait
