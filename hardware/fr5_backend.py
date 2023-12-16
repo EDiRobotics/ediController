c97eacd97047fad0503af92c909bf798e7dcf814
radiance-nt <1808154289@qq.com>
2023-12-16 22:04:06 +0800

    <update> finish dagger

--- hardware/fr5_backend.py
+++ hardware/fr5_backend.py
@@ -60,15 +60,13 @@ idx = 0
 
 
 def handle_service(request: StringServiceRequest):
-    global idx
-    rospy.logdebug(f"[Backend] Respond to request: {request}")
-
+    global idx, last_switch_state
     try:
         idx += 1
         publisher_idx.publish(idx)
         action_str = request.message
         action = json.loads(action_str)
-        action_record = {"_action": action, "_timestamp": rospy.Time.now().to_time(), "_mode": 0}
+        action_record = {"action": action, "timestamp": rospy.Time.now().to_time(), "mode": last_switch_state}
         publisher_action.publish(json.dumps(action_record))
         info = step_with_action(action)
         info_str = json.dumps(info)
--- start_demo.sh
+++ start_demo.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-source /opt/ros/noetic/setup.bash
+#source /opt/ros/noetic/setup.bash
+source catkin_ws/devel/setup.bash
 export ROS_HOSTNAME=192.168.1.240; export ROS_MASTER_URI=http://192.168.1.240:11311
 echo "Set ROS_HOSTNAME=${ROS_HOSTNAME}, ROS_MASTER_URI=${ROS_MASTER_URI}..."
 
-echo "Starting Real World Interface..."
-python data_collection/collect.py &
-
-sleep 5
 echo "Starting Demo Arm Publisher..."
 python hardware/arm_demo.py &
 
