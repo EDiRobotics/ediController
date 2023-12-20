98031dddb19fd071362b3d193f7239bbea353797
radiance-nt <1808154289@qq.com>
2023-12-21 00:07:17 +0800

    <update> recorder

--- data_collection/lmdb_saver.py
+++ data_collection/lmdb_saver.py
@@ -1,4 +1,27 @@
 # TODO: Save data to LMDB Datasets
 
-def save_to_lmdb(results):
+def save_to_lmdb(records):
+    """
+    records:
+    {
+        "base_timestamp": 1703088159.7284524,
+        "records": [
+            {
+                "action": [23.0, -113.0, -102.0, -54.0, 90.0, -170.0],
+                "obs": {
+                    "status": {"s1": 0},
+                    "images": {"/camera1": np.ndarray}
+                },
+                "obs_timestamp": 1703088259.7284524,
+                "action_timestamp": 1703088359.7284524,
+                "action_mode": "demo",
+            },
+            # ... Additional records
+        ],
+        "max_step": 200
+    }
+    :param results:
+    :return:
+    """
+
     pass
